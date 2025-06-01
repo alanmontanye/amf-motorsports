d50e14dcf217bc04b0b0d930fe57a1a6a3a41dbd
alanmontanye <montanyealan@gmail.com>
2025-06-01 16:27:38 -0500

    Fix blueprint import errors: remove non-existent parts_bp import and fix circular imports using relative imports

--- app/atv/__init__.py
+++ app/atv/__init__.py
@@ -4,14 +4,13 @@ bp = Blueprint('atv', __name__, url_prefix='/atv')
 
 # Import all route modules
 from . import routes
-from .parts import parts_bp
+from . import parts
 from .parting import parting_bp
 from .parts_bulk import parts_bulk_bp
 from .quick_edit import quick_edit_bp
-from app.atv import storage, ebay, ebay_settings, ebay_analytics, ebay_api, ebay_routes, ebay_dashboard, ai_description
+from . import storage, ebay, ebay_settings, ebay_analytics, ebay_api, ebay_routes, ebay_dashboard, ai_description
 
 # Register blueprints
-bp.register_blueprint(parts_bp)
 bp.register_blueprint(parting_bp)
 bp.register_blueprint(parts_bulk_bp)
 bp.register_blueprint(quick_edit_bp)
